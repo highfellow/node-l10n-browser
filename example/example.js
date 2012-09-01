d17faaaae85dd1e7e01df13084ce972f1f7678a4
Andy Baxter <highfellow@gmail.com>
2012-09-01 19:07:27 +0100

    pink penguins

--- example/example.js
+++ example/example.js
@@ -16,24 +16,25 @@ function appendElt(elt, type, content) {
 
 // run through plurals using a given adjective.
 function repeat(elt,adjKey) {
-  var adj=l10n.get(adjKey,{},'colour');
+  var adj;
   for (var n = 0; n < 4; n++) {
+    adj=l10n.get(adjKey, {'n':n}, 'colour');
     appendElt(elt,'li',l10n.get('phrase',{'n':n, 'adj':adj}, 'brown fox phrase'));
   }
 }
 
 // test in a given language.
 function testLanguage(lang, callback) {
-  var h2 = appendElt(outputElt,'h2','Language:' + lang)
+  var h2 = appendElt(outputElt, 'h2', 'Language:' + lang)
   l10n.loadResource('data.properties', lang, function() {
-    var h3 = appendElt(h2,'h3','Usual version');
-    var p = appendElt(h2,'p','');
-    var ul = appendElt(p,'ul','');
-    repeat(ul,'brown');
-    h3 = appendElt(h2,'h3','With parameter substitution');
-    p = appendElt(h2,'p','');
-    ul = appendElt(p,'ul','');
-    repeat(ul,'pink');
+    var h3 = appendElt(h2, 'h3', 'Usual version');
+    var p = appendElt(h2, 'p', '');
+    var ul = appendElt(p, 'ul', '');
+    repeat(ul, 'brown');
+    h3 = appendElt(h2, 'h3', 'With parameter substitution');
+    p = appendElt(h2, 'p', '');
+    ul = appendElt(p, 'ul', '');
+    repeat(ul, 'pink');
     callback && callback();
   },
   function(err) {
