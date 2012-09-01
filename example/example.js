cf49a3b1cc21f8aecbcff9c7ec08bc17cc54c1fb
Andy Baxter <highfellow@gmail.com>
2012-09-01 11:00:14 +0100

    Getting example to work nicely in html

--- example/example.js
+++ example/example.js
@@ -4,27 +4,36 @@
 
 var L10n = require('l10n');
 var L10n_Browser = require('../l10n-browser.js');
+var outputElt = null; // element to write output into.
+
+function appendElt(elt, type, content) {
+  // add an element to elt and fill it.
+  var newElt = document.createElement(type);
+  newElt.textContent = content;
+  elt.appendChild(newElt);
+  return(newElt);
+}
 
 // run through plurals using a given adjective.
-function repeat(adjKey) {
-  adj=l10n.get(adjKey,{},'colour');
+function repeat(elt,adjKey) {
+  var adj=l10n.get(adjKey,{},'colour');
   for (var n = 0; n < 4; n++) {
-    console.log("  ",l10n.get('phrase',{'n':n, 'adj':adj}, 'brown fox phrase'));
+    appendElt(elt,'li',l10n.get('phrase',{'n':n, 'adj':adj}, 'brown fox phrase'));
   }
 }
 
 // test in a given language.
 function testLanguage(lang, callback) {
-  console.log();
-  console.log('Testing in language:', lang);
-  console.log('=======================');
-  console.log();
+  var h2 = appendElt(outputElt,'h2','Language:' + lang)
   l10n.loadResource('data.properties', lang, function() {
-    console.log('Standard version:');
-    repeat('brown');
-    console.log();
-    console.log('Testing parameter substitution:');
-    repeat('pink');
+    var h3 = appendElt(h2,'h3','Usual version');
+    var p = appendElt(h2,'p','');
+    var ul = appendElt(p,'ul','');
+    repeat(ul,'brown');
+    h3 = appendElt(h2,'h3','With parameter substitution');
+    p = appendElt(h2,'p','');
+    ul = appendElt(p,'ul','');
+    repeat(ul,'pink');
     callback && callback();
   },
   function(err) {
@@ -32,10 +41,14 @@ function testLanguage(lang, callback) {
   });
 }
 
+window.onload = function() {
+  outputElt = document.getElementById('output'); 
+  // do the tests.
+  testLanguage('en', function() {
+    testLanguage('de');
+  });
+}
+
 // initialise L10n with the file adapter.
 var l10n = new L10n(new L10n_Browser());
 
-// do the tests.
-testLanguage('en', function() {
-  testLanguage('de');
-});
